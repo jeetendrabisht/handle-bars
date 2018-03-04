070c2bc50c70cba37173eae710b99315aef5cd4e
jeetendrabisht <jeegarbisht@gmail.com>
2018-03-05 01:40:16 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+app.set('views', '../views');
+
+hbs.registerPartials('../views/partials');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} :: ${req.method} :: ${req.url}`;
+  fs.appendFile('server.log',`${log} \n`,(err) => {
+    if(err) {
+      console.log('Error while logging file');
+    }
+    console.log(`${now} :: ${req.method} :: ${req.url}`);
+    next();
+  });
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs',{
+//     'message' : 'Website is in maintenance phase...'
+//   });
+// });
+
+app.use(express.static('../public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('fullCase',(text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', function(req, res) {
+  res.render('initial.hbs',{
+    'message': 'Main Page'
+  });
+});
+
+app.get('/home',function(req, res) {
+  res.render('home.hbs',{
+   'message' : "WELCOME TO NEW WORLD"
+  });
+});
+
+app.get('/about',function(req, res) {
+  res.render('about.hbs', {
+    'message' : 'Managing Environment'
+  });
+});
+
+app.listen(3000, function (err) {
+  if(err) {
+    console.log("Error While Connecting :: ", err);
+  }
+  console.log("Server listening at port 3000");
+});
